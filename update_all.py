e3066ebaa2a101777cb316e9168bf72aa8d36ec8
Erik Schake <45861363+erikjs14@users.noreply.github.com>
2022-02-07 22:02:22 +0200

    update update_all.py

--- update_all.py
+++ update_all.py
@@ -5,9 +5,9 @@ import pytz
 first_date_str = '2022-02-01'
 today = datetime.now(pytz.timezone('Europe/Berlin'))
 
-cur_date = today
-while cur_date.strftime('%Y-%m-%d') >= first_date_str:
+cur_date = datetime.strptime(first_date_str, '%Y-%m-%d').date()
+while cur_date.strftime('%Y-%m-%d') <= today.strftime('%Y-%m-%d'):
   cur_date_str = cur_date.strftime('%Y-%m-%d')
   os.environ['DTP'] = cur_date_str
   os.system('jupyter nbconvert --ExecutePreprocessor.timeout=500 --to notebook --execute update.ipynb')
-  cur_date = cur_date + timedelta(-1)
+  cur_date = cur_date + timedelta(1)
